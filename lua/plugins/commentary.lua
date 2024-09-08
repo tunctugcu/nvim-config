a11bd1d2653b054e8ef4576168ebe9f058bdeaf8
Tunc Tugcu <tunctugcu@gmail.com>
2024-09-09 01:09:27 +0300

    feat: comment for ts added

--- lua/plugins/commentary.lua
+++ lua/plugins/commentary.lua
@@ -1,3 +1,11 @@
 return {
-	'tpope/vim-commentary'
+	'tpope/vim-commentary',
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
+  config = function ()
+    require('ts_context_commentstring').setup {
+      enable_autocmd = false,
+    }
+  end
 }
