b97fdfa5a311a3cceba454f0009eb1581476936d
Tunc Tugcu <tunctugcu@gmail.com>
2024-09-08 04:36:22 +0300

    feat: rest plugin added

---
+++ lua/plugins/rest.lua
@@ -0,0 +1,12 @@
+return {
+  'diepm/vim-rest-console',
+  config = function ()
+    vim.g.vrc_set_default_mapping = 0
+    vim.g.vrc_response_default_content_type = 'application/json'
+    vim.g.vrc_output_buffer_name = '_OUTPUT.json'
+    vim.g.vrc_auto_format_response_patterns = {
+      json = 'jq'
+    }
+   vim.keymap.set('n', '<leader>xr', ":call VrcQuery()<CR>")
+  end
+}
