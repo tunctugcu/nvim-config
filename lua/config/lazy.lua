d4e2d91a85d6e3fb9256dd4a0d915507b51a9ca2
Tunc Tugcu <tunctugcu@gmail.com>
2024-09-09 00:40:01 +0300

    fix: move multiple lines fixed

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -50,14 +50,13 @@ vim.opt.splitright = true
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "<C-J>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-K>", ":m '<-2<CR>gv=gv")
 
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(e)
 		local opts = { buffer = e.buf }
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
 	-- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
