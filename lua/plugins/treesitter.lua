264603036be7bc6769b84cb9b82f2178c17cd8a2
Tunc Tugcu <tunctugcu@gmail.com>
2024-09-28 22:48:36 +0300

    auto tag bug fixe

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,10 +13,6 @@ return {
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
-      autotag = {
-        enable = true,
-      },
-
       highlight = {
         enable = true,
 
