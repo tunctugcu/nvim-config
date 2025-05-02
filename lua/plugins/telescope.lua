7b970d8100fdc106e47778755e38aa458cada52b
Tunc Tugcu <tunctugcu@gmail.com>
2025-05-03 00:47:43 +0300

    update: telescope focus handled windsuft installed

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,10 +3,24 @@ return {
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup({
+      defaults = {
+        file_ignore_patterns = {"node_modules", "%.git", "dist", "build"}
+      }, {
+        pickers = {
+          find_files = {
+            hidden = true
+          }
+        }
+      }
+    })
     local builtin = require('telescope.builtin')
 
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+    
+
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {  })
     vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>ps', function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
     end)
